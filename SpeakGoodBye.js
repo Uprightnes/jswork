a3ef980c48a98a3b3243eed7a7c94d3f25e09089
uprightnes <eziukwuuprightness@gmail.com>
2024-03-05 09:33:45 +0100

    commit

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -5,21 +5,63 @@
 // See Lecture 52, part 2
 
 
-// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
-// var byeSpeaker =
+(function (window) {
+    var yaakovGreeter = {};
+    yaakovGreeter.name = 'Yaakov';
+    yaakovGreeter.greeting = 'Hello ';
+    yaakovGreeter.sayHello = function () {
+        console.log(yaakovGreeter.greeting + yaakovGreeter.name);
+    }
 
-// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+    window.yaakovGreeter = yaakovGreeter;
 
-// STEP 8: Rewrite the 'speak' function such that it is attached to the
-// byeSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+})(window);
 
-// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-// 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+(function (window) {
+    var johnGreeter = {};
+    johnGreeter.name = 'John';
+    johnGreeter.greeting = 'Hi ';
+    johnGreeter.sayHi = function () {
+        console.log(johnGreeter.greeting + johnGreeter.name);
+    }
+
+    window.johnGreeter = johnGreeter;
+
+})(window);
+
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+
+
+
+
+
+// (function(window){
+
+// // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// // the "speak" method and which you will expose to the global context
+// // See Lecture 52, part 1
+
+// var byeSpeaker = {};
+// byeSpeaker.name = name;
+// var speakWord = "Good Bye";
+
+
+// byeSpeaker.speak = function (name) {
+//   console.log(speakWord + " " + name);
+// }
+
+// // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+
+
+// // STEP 8: Rewrite the 'speak' function such that it is attached to the
+// // byeSpeaker object instead of being a standalone function.
+// // See Lecture 52, part 2
+
+// window.byeSpeaker = byeSpeaker;
+// })(window);
+
+// // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// // 'byeSpeaker' on the global scope as well.
+// // xxxx.xxxx = byeSpeaker;
